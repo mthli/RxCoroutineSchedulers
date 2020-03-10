37e243c57e98e5f8c8367d389c86b4a7018cc625
李明亮 <matthewlee0725@gmail.com>
2020-03-10 23:51:00 +0800

    feat: setup project

---
+++ src/main/java/io/github/mthli/rxcoroutineschedulers/CoroutineWorker.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2020 Matthew Lee
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.mthli.rxcoroutineschedulers
+
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.Disposable
+import java.util.concurrent.TimeUnit
+
+internal class CoroutineWorker : Scheduler.Worker() {
+    override fun isDisposed(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun schedule(run: Runnable?, delay: Long, unit: TimeUnit?): Disposable {
+        TODO("Not yet implemented")
+    }
+
+    override fun dispose() {
+        TODO("Not yet implemented")
+    }
+}
